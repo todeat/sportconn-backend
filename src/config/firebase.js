5fc0dc7baa1e9d57e8ef38d0fb2a57906a46e154
Tudor Todea <tudortodea@gmail.com>
2024-12-28 16:41:35 +0200

    service account added to env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 // config/firebase.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
